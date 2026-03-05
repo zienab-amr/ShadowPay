6f3ec38910a7a67b75a5c11b603ab8e3718809c8
Fatma Saeed <fatmasaeedd111@gmail.com>
2026-03-05 21:40:29 +0200

    splash scrren

--- shadow_pay_app/lib/main.dart
+++ shadow_pay_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/onboarding_screen.dart'; // استخدمي المسار الصحيح لملف onboarding واحد فقط
+import 'splash_screen.dart';
+import 'screens/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const OnboardingScreen(), // استخدمي اسم الشاشة المناسب هنا
+      home: const SplashScreen(),
     );
   }
 }
