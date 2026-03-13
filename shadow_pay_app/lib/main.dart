c5d6016c9bedff0520d90aab8011d56905b3d7cc
Martina Sameh <martinasameh012@gmail.com>
2026-03-13 15:26:50 +0200

    register and lang

--- shadow_pay_app/lib/main.dart
+++ shadow_pay_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'splash_screen.dart';
-import 'screens/onboarding_screen.dart';
-
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'Register/register_screen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -12,6 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+       locale: Locale('en'),
+      supportedLocales: [
+    Locale('en'),
+    Locale('ar'),
+  ],
+  localizationsDelegates: [
+    GlobalMaterialLocalizations.delegate,
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+  ],
       title: 'ShadowPay',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
